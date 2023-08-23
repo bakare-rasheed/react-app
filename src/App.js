469d388e7a72e36edc41e9fd6002640f2963cab3
bakare-rasheed <bakarerasheed.a38@gmail.com>
2023-08-23 17:55:34 +0100

    buildspec

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>This is bakare's Website!</h2>
+      <h2>This is bakare's Website!..... welcome</h2>
     </div>
   );
 }
